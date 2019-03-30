55e5b16f2a74d583b088d5534b39a9588587a39c
Danielle <daniellezelin@gmail.com>
2019-03-30 19:20:18 -0400

    Added test cases for models

--- log-management-api/test/models/log_test.rb
+++ log-management-api/test/models/log_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class LogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    def setup
+        @log = logs(:valid)
+    end
+
+    test 'valid log' do
+        assert @log.valid?
+    end
+
+    test 'invalid without log_time' do
+        @log.log_time = nil
+        refute @log.valid?, 'saved log without a timestamp'
+        assert_not_nil @log.errors[:log_time], 'no validation error for log_time present'
+    end
+
+    test 'invalid without log_data' do
+        @log.log_data = nil
+        refute @log.valid?, 'log is valid without data'
+        assert_not_nil @log.errors[:log_data], 'no validation error for log_data present'
+    end
 end
