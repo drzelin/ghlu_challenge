66c0bc50e17e14d3195e0efd6e311376d44bb3f0
Danielle <daniellezelin@gmail.com>
2019-03-30 17:51:58 -0400

    Added status codes for requesting logs within a range

--- log-management-api/app/controllers/api/v1/request_logs_controller.rb
+++ log-management-api/app/controllers/api/v1/request_logs_controller.rb
@@ -11,10 +11,14 @@ module Api
       end
 
       def show
-        @start = DateTime.parse(params[:start]).iso8601(6)
-        @end = DateTime.parse(params[:end]).iso8601(6)
-        log = Log.where('log_time BETWEEN ? AND ?', @start, @end)
-        render json: log 
+        begin
+            start = DateTime.parse(params[:start]).iso8601(6)
+            finish = DateTime.parse(params[:end]).iso8601(6)
+            logs = Log.where('log_time BETWEEN ? AND ?', start, finish)
+            render json: {status: 'SUCCESS', message: 'Loaded logs', data: logs}, status: :ok
+        rescue ArgumentError
+            render json: {status: 'ERROR', message: 'Did not load logs', data: params}, status: :bad_request
+        end
       end
 
     end
