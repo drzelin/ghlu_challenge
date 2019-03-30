032ef53ad8d8301579516ee1d12cb47c5e832221
Danielle Zelin <dzelin@mitre.org>
2019-03-30 11:13:09 -0400

    Validated database and inserted dummy date, TODO: use faker to create dummy random date

--- log-management-api/app/models/log.rb
+++ log-management-api/app/models/log.rb
@@ -1,2 +1,4 @@
 class Log < ApplicationRecord
+  validates :log_data, presence: true
+  validates :log_time, presence: true
 end
--- log-management-api/db/seeds.rb
+++ log-management-api/db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  Log.create({
+    log_data: "heroku[router]: at=info method=POST path=\"/v1/posts\" host=api.example.com request_id=12345 dyno=web.1 connect=0ms service=100ms status=200 bytes=358",
+    log_time: "2016-02-11T23:05:03.803616+00:00"
+    #
+    # TODO: use Faker
+    #log_data: Faker::Lorem.sentence 
+    #log_time: Faker::Time.between(2.days.ago, Time.now, :all)       #=> "2014-09-19 07:03:30 -0700"
+
+  })
+end
